c3c774471058022cc8a55a8f71fb6d6c17b0343b
rahdy <rahdy@code-rhapsodie.fr>
2023-07-28 10:40:42 +0200

    homeController

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -2,17 +2,48 @@
 
 namespace App\Controller;
 
+use App\Entity\User;
+use App\Repository\ProductsRepository;
+use App\Repository\TransactionsRepository;
+use App\Repository\UserRepository;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\ParamConverter;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 class HomeController extends AbstractController
 {
-    #[Route('/home', name: 'app_home')]
-    public function index(): Response
+    #[Route('/home/{id}', name: 'app_home', methods: ['GET'])]
+	#[ParamConverter('user' ,  User::class)]
+    public function index(TransactionsRepository $transactionsRepository, ProductsRepository $productsRepository,User $user ): Response
     {
-        return $this->render('home/index.html.twig', [
-            'controller_name' => 'HomeController',
-        ]);
-    }
+		// Récupérer l'utilisateur actuel
+		$User = $this->getUser();
+
+		// Récupérer le total des transactions par especes
+		$totalCashAmount = $transactionsRepository->getTotalCashAmount();
+		// Récupérer le total des transactions par carte bancaire
+		$totalCardAmount = $transactionsRepository->getTotalCardAmount();
+		// Récupérer le total des transactions en chèques
+		$totalChequeAmount = $transactionsRepository->getTotalChequeAmount();
+		// Récupérer le total des transactions
+		$totalEncaisse = $totalChequeAmount + $totalCardAmount + $totalCashAmount;
+		// Récupérer les 5 derniers produits ajoutés
+		$latestProducts = $productsRepository->findBy([], ['id' => 'DESC'], 5);
+		// Récupérer les 5 dernières transactions avec les IDs les plus grands
+		$transactions = $transactionsRepository->findBy([], ['id' => 'DESC'], 5);
+
+		return $this->render('home/index.html.twig', [
+			'controller_name' => 'DashboardController',
+			'totalCashAmount' => $totalCashAmount,
+			'totalCardAmount' => $totalCardAmount,
+			'totalChequeAmount' => $totalChequeAmount,
+			'totalEncaisse' => $totalEncaisse,
+			'latestProducts' => $latestProducts,
+			'transactions' => $transactions,
+			'User' => $User,
+		]);
+
+	}
+
 }
